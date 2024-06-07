fix wrong index slicing in binary classification visualization

nca/visualization.py
@@ -46,7 +46,7 @@ def show_batch_binary_image_classification(x_seed, x_pred, y_true, nca):
 
     # 2nd row: prediction
     images = np.ones((batch_size, image_width, image_height))
-    class_channels = x_pred[..., : -nca.num_output_channels]
+    class_channels = x_pred[..., nca.num_image_channels + nca.num_hidden_channels :]
     y_pred = np.argmax(class_channels, axis=-1)
     images = (x_seed[:, :, :, 0] > 0).astype(np.float32)
     for i in range(batch_size):
@@ -85,7 +85,8 @@ def show_batch_classification(x_seed, x_pred, y_true, nca):
         ax[1, j].text(0, 1, f"true: {y_true[j][0]}")
         ax[1, j].text(0, 0.9, f"pred: {y_pred[j]}")
         ax[1, j].axis("off")
-    
+
+    # 3rd row: activity
     images = hidden_channels[..., 0]
     show_image_row(ax[2], images)
 