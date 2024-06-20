fix alive masking

nca/models/basicNCA.py
@@ -85,10 +85,20 @@ class BasicNCAModel(nn.Module):
     def alive(self, x):
         mask = (
             F.max_pool2d(
-                x[:, self.num_image_channels, :, :], kernel_size=3, stride=1, padding=1
+                x[
+                    :,
+                    self.num_image_channels : self.num_image_channels
+                    + self.num_hidden_channels,
+                    :,
+                    :,
+                ],
+                kernel_size=3,
+                stride=1,
+                padding=1,
             )
             > 0.1
         )
+        mask = torch.any(mask, dim=1)
         return mask
 
     def perceive(self, x):
@@ -109,9 +119,17 @@ class BasicNCAModel(nn.Module):
         y = torch.cat(perception, 1)
         return y
 
-    def update(self, x):
-        x = x.transpose(1, 3)
-        pre_life_mask = self.alive(x)
+    def update(self, x, step=0):
+        x = x.transpose(1, 3)  # B W H C --> B C W H
+
+        hidden_channels = x[
+            :,
+            self.num_image_channels : self.num_image_channels
+            + self.num_hidden_channels,
+            :,
+            :,
+        ]
+        pre_life_mask = self.alive(hidden_channels)
 
         # Perception
         dx = self.perceive(x)
@@ -121,28 +139,31 @@ class BasicNCAModel(nn.Module):
         dx = self.network(dx)
 
         # Stochastic weight update
+        fire_rate = self.fire_rate
         stochastic = (
-            torch.rand([dx.size(0), dx.size(1), dx.size(2), 1]) > self.fire_rate
+            torch.rand([dx.size(0), dx.size(1), dx.size(2), 1]) > fire_rate
         )
         stochastic = stochastic.float().to(self.device)
         dx = dx * stochastic
         if self.immutable_image_channels:
             dx[..., : self.num_image_channels] *= 0
 
-        # Alive masking
         x = x + dx.transpose(1, 3)  # B W H C --> B C W H
-        # FIXME: Something is wrong in the state of Denmark
+
+        # Alive masking
+        life_mask = self.alive(hidden_channels)
+        life_mask = life_mask & pre_life_mask
+
         if self.use_alive_mask:
-            x = x.transpose(0, 1)
-            life_mask = self.alive(x) & pre_life_mask
+            x = x.transpose(0, 1)  # B C W H --> C B W H
             x = x * life_mask.float()
-            x = x.transpose(1, 0)
+            x = x.transpose(1, 0)  # C B W H --> B C W H
         x = x.transpose(1, 3)  # B C W H --> B W H C
         return x
 
     def forward(self, x, steps: int = 1):
-        for _ in range(steps):
-            x = self.update(x)
+        for step in range(steps):
+            x = self.update(x, step)
         return x
 
     def loss(self, x, target):