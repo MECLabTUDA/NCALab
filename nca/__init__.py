00c6261247d82aa185db5461595808b77a1c7b65
Henry Krumb <mail@henrykrumb.de>
2024-07-11 14:08:50 +0200

    add BasicNCATrainer to module-wide imports

--- nca/__init__.py
+++ nca/__init__.py
@@ -1,6 +1,6 @@
 from .data import GrowingNCADataset
 from .models import *
 from .paths import *
-from .training import train_basic_nca
+from .training import BasicNCATrainer
 from .utils import get_compute_device, pad_input
 from .visualization import *
