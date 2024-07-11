25bc3df57114199271934ac61c8c8392e3f36b1f
Henry Krumb <mail@henrykrumb.de>
2024-07-11 15:27:27 +0200

    remove old reference to taskNCA

--- nca/models/__init__.py
+++ nca/models/__init__.py
@@ -2,4 +2,3 @@ from .basicNCA import BasicNCAModel  # noqa: F401
 from .classificationNCA import ClassificationNCAModel  # noqa: F401
 from .growingNCA import GrowingNCAModel  # noqa: F401
 from .segmentationNCA import SegmentationNCAModel  # noqa: F401
-from .taskNCA import TaskNCAModel  # noqa: F401
