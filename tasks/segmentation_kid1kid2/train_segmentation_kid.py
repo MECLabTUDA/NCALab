421824f4d98e6498c223723bb019a745ad5ea528
Henry Krumb <mail@henrykrumb.de>
2024-08-15 09:48:58 +0200

    implement validation in KID2 example
    - use train / val / test split stored in custom csv file
    - store best model

--- tasks/segmentation_kid1kid2/train_segmentation_kid.py
+++ tasks/segmentation_kid1kid2/train_segmentation_kid.py
@@ -14,23 +14,23 @@ import click
 import torch
 import numpy as np
 
-import albumentations as A
-from albumentations.pytorch import ToTensorV2
+import albumentations as A  # type: ignore[import-untyped]
+from albumentations.pytorch import ToTensorV2  # type: ignore[import-untyped]
 
 from torch.utils.tensorboard import SummaryWriter
 from torch.utils.data import Dataset, Subset
 from PIL import Image
 
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split  # type: ignore[import-untyped]
 
 from config import KID_DATASET_PATH
 
 
 class KIDDataset(Dataset):
-    def __init__(self, path: Path | PosixPath, transform=None) -> None:
+    def __init__(self, path: Path | PosixPath, filenames, transform=None) -> None:
         super().__init__()
         self.path = path
-        self.image_filenames = sorted(os.listdir(str(path / "vascular")))
+        self.image_filenames = filenames
         self.transform = transform
 
     def __len__(self):
@@ -63,6 +63,7 @@ def train_segmentation_KID(batch_size: int, hidden_channels: int):
         num_image_channels=3,
         num_hidden_channels=hidden_channels,
         num_classes=1,
+        lambda_activity=0.00
     )
 
     T = A.Compose(
@@ -73,28 +74,30 @@ def train_segmentation_KID(batch_size: int, hidden_channels: int):
             ToTensorV2(),
         ]
     )
-    dataset = KIDDataset(KID_DATASET_PATH, transform=T)
-
-    train_indices, val_indices, _, _ = train_test_split(
-        range(len(dataset)),
-        dataset.image_filenames,
-        test_size=int(len(dataset) * 0.2),
+    import pandas as pd
+    split = pd.read_csv(KID_DATASET_PATH / "split_vascular.csv")
+    train_dataset = KIDDataset(
+        KID_DATASET_PATH,
+        filenames=split[split.split == "train"].filename.values,
+        transform=T,
     )
-
-    train_split = Subset(dataset, train_indices)
-    val_split = Subset(dataset, val_indices)
+    val_dataset = KIDDataset(
+        KID_DATASET_PATH,
+        filenames=split[split.split == "val"].filename.values,
+        transform=T,
+    )
 
     loader_train = torch.utils.data.DataLoader(
-        train_split, shuffle=True, batch_size=batch_size, drop_last=True
+        train_dataset, shuffle=True, batch_size=batch_size, drop_last=True
     )
     loader_val = torch.utils.data.DataLoader(
-        val_split, shuffle=True, batch_size=batch_size, drop_last=True
+        val_dataset, shuffle=True, batch_size=batch_size, drop_last=True
     )
 
-    trainer = BasicNCATrainer(nca, WEIGHTS_PATH / "segmentation_KID2_vascular.pth")
+    trainer = BasicNCATrainer(nca, WEIGHTS_PATH / "segmentation_KID2_vascular.pth", max_epochs=1000)
     trainer.train_basic_nca(
         loader_train,
-        # loader_val,
+        loader_val,
         summary_writer=writer,
     )
     writer.close()
@@ -102,7 +105,7 @@ def train_segmentation_KID(batch_size: int, hidden_channels: int):
 
 @click.command()
 @click.option("--batch-size", "-b", default=8, type=int)
-@click.option("--hidden-channels", "-H", default=14, type=int)
+@click.option("--hidden-channels", "-H", default=16, type=int)
 def main(batch_size, hidden_channels):
     train_segmentation_KID(batch_size=batch_size, hidden_channels=hidden_channels)
 
