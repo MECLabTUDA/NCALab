fix normal distribution scaling in pad_input with noise

ncalab/utils.py
@@ -22,7 +22,7 @@ def pad_input(x, nca, noise=True):
                 :,
                 :,
             ] = torch.normal(
-                0, 1, size=(x.shape[0], nca.num_hidden_channels, x.shape[2], x.shape[3])
+                0.5, 0.225, size=(x.shape[0], nca.num_hidden_channels, x.shape[2], x.shape[3])
             )
     return x
 