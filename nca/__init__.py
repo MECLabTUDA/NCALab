cdcc4e46de801c16a1b7846259a25bb532eea2cf
Henry Krumb <mail@henrykrumb.de>
2024-11-01 09:13:15 +0100

    remove old 'nca' directory from repo, dunno how it got there

--- nca/__init__.py
+++
@@ -1,6 +0,0 @@
-from .data import GrowingNCADataset  # noqa: F401
-from .models import *  # noqa: F401,F403
-from .paths import *  # noqa: F401,F403
-from .training import BasicNCATrainer  # noqa: F401
-from .utils import get_compute_device, NCALab_banner, pad_input  # noqa: F401
-from .visualization import *  # noqa: F401,F403
--- nca/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from .basicNCA import BasicNCAModel  # noqa: F401
-from .classificationNCA import ClassificationNCAModel  # noqa: F401
-from .growingNCA import GrowingNCAModel  # noqa: F401
-from .segmentationNCA import SegmentationNCAModel  # noqa: F401
