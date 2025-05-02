4983922ce0b382d89feb88c18c563dae12a7e132
Henry Krumb <mail@henrykrumb.de>
2025-05-02 18:12:46 +0200

    stop exposing visualization functions in ncalab module

--- ncalab/__init__.py
+++ ncalab/__init__.py
@@ -6,4 +6,3 @@ from .paths import *  # noqa: F401,F403
 from .search import *  # noqa: F401,F403
 from .training import *  # noqa: F401
 from .utils import *  # noqa: F401
-from .visualization import *  # noqa: F401,F403
