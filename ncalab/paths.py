8ae3c3e8bef00ca981d22d973a58f7f3196ef1a9
Henry Krumb <mail@henrykrumb.de>
2025-05-02 17:53:42 +0200

    document paths.py

--- ncalab/paths.py
+++ ncalab/paths.py
@@ -1,9 +1,23 @@
 from pathlib import Path
 
+"""
+Current file directory.
+"""
+__CURRENT_PATH = Path(__file__).resolve().parent
 
-CURRENT_PATH = Path(__file__).resolve().parent
-ROOT_PATH = (CURRENT_PATH / "..").absolute()
+"""
+Project root directory.
+"""
+ROOT_PATH = (__CURRENT_PATH / "..").absolute()
+
+"""
+Directory in which training weights are stored.
+"""
 WEIGHTS_PATH = ROOT_PATH / "weights"
+
+"""
+Example task directory.
+"""
 TASK_PATH = ROOT_PATH / "tasks"
 
 WEIGHTS_PATH.mkdir(exist_ok=True, parents=True)
