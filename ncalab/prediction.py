ae86195a44af2ed313f66c8b1b9480c07dec7e6f
Henry Krumb <mail@henrykrumb.de>
2025-08-14 10:58:32 +0200

    write proper doc strings for prediction.py

--- ncalab/prediction.py
+++ ncalab/prediction.py
@@ -7,16 +7,23 @@ import torch
 class Prediction:
     """
     Stores the result of an NCA prediction, including the number of steps it took.
+
+    Sequences are typically stored by BasicNCAModel's "record" function, and are
+    returned as a list of Prediction objects.
     """
 
     def __init__(self, model, steps: int, output_image: torch.Tensor):
         """
-        Constructor. Not called explicitly; forward pass of BasicNCAModel (and its
-        subclasses) is responsible for filling the attributes.
+        Constructor is typically not called explicitly.
+        Rather, the forward pass of BasicNCAModel (and its
+        subclasses) is responsible for filling its attributes.
 
-        :param model [BasicNCAModel]: Reference to model used for prediction.
-        :param steps [int]: Number of steps taken for the prediction.
-        :param output_image [torch.Tensor]: Output image tensor.
+        :param model: Reference to model used for prediction.
+        :type model: ncalab.BasicNCAModel
+        :param steps: Number of steps taken for the prediction.
+        :type steps: int
+        :param output_image: Output image tensor.
+        :type output_image: torch.Tensor
         """
         self.model = model
         self.steps = steps
@@ -27,14 +34,20 @@ class Prediction:
     @property
     def image_channels(self) -> torch.Tensor:
         """
-        :returns [torch.Tensor]: BCWH
+        Convenience property to access the image channels as a Tensor.
+
+        :returns: BCWH Tensor
+        :rtype: torch.Tensor
         """
         return self.output_image[:, : self.model.num_image_channels, :, :]
 
     @property
     def hidden_channels(self) -> torch.Tensor:
         """
-        :returns [torch.Tensor]: BCWH
+        Convenience property to access the hidden channels as a Tensor.
+
+        :returns: BCWH Tensor
+        :rtype: torch.Tensor
         """
         return self.output_image[
             :,
@@ -47,7 +60,10 @@ class Prediction:
     @property
     def output_channels(self) -> torch.Tensor:
         """
-        :returns [torch.Tensor]: BCWH
+        Convenience property to access the output channels as a Tensor.
+
+        :returns: BCWH Tensor
+        :rtype: torch.Tensor
         """
         return self.output_image[
             :,
@@ -59,7 +75,12 @@ class Prediction:
     @property
     def output_array(self) -> np.ndarray:
         """
-        :returns [np.ndarray]: BCWH
+        Convenience property to access the whole output image in the format of
+        a numpy array. Brings the entire tensor to CPU on demand, and only at
+        the first call.
+
+        :returns: Numpy array in BCWH format
+        :rtype: np.ndarray
         """
         if self._output_array is None:
             self._output_array = self.output_image.detach().cpu().numpy()
@@ -68,7 +89,12 @@ class Prediction:
     @property
     def image_channels_np(self) -> np.ndarray:
         """
-        :returns [np.ndarray]: BCWH
+        Convenience property to access the output image channels in the format of
+        a numpy array. Brings the entire tensor to CPU on demand, and only at
+        the first call.
+
+        :returns: Numpy array in BCWH format
+        :rtype: np.ndarray
         """
         if self._output_array is None:
             self._output_array = self.output_image.detach().cpu().numpy()
@@ -77,7 +103,12 @@ class Prediction:
     @property
     def hidden_channels_np(self) -> np.ndarray:
         """
-        :returns [np.ndarray]: BCWH
+        Convenience property to access the hidden image channels in the format of
+        a numpy array. Brings the entire tensor to CPU on demand, and only at
+        the first call.
+
+        :returns: Numpy array in BCWH format
+        :rtype: np.ndarray
         """
         if self._output_array is None:
             self._output_array = self.output_image.detach().cpu().numpy()
@@ -92,7 +123,12 @@ class Prediction:
     @property
     def output_channels_np(self) -> np.ndarray:
         """
-        :returns [np.ndarray]: BCWH
+        Convenience property to access the image's output channels in the format of
+        a numpy array. Brings the entire tensor to CPU on demand, and only at
+        the first call.
+
+        :returns: Numpy array in BCWH format
+        :rtype: np.ndarray
         """
         if self._output_array is None:
             self._output_array = self.output_image.detach().cpu().numpy()
