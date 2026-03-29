bb39e70842aec71fceeefac61b1c582e89735385
Henry Krumb <mail@henrykrumb.de>
2026-03-29 22:20:47 +0200

    fix variable names in mlp rule

--- ncalab/models/__init__.py
+++ ncalab/models/__init__.py
@@ -1,5 +1,3 @@
 from .basicNCA import *  # noqa: F401,F403
-from .extremeNCA import *  # noqa: F401,F403
-from .rbfNCA import *  # noqa: F401,F403
 from .wrappers import *  # noqa: F401,F403
 from .applications import *  # noqa: F401,F403
--- ncalab/models/basicNCA/abstractNCArule.py
+++ ncalab/models/basicNCA/abstractNCArule.py
@@ -6,12 +6,12 @@ from torch import nn
 
 class AbstractNCARule(nn.Module, abc.ABC):
     def __init__(
-        self, device: torch.device, input_size: int, latent_size: int, output_size: int
+        self, device: torch.device, input_size: int, hidden_size: int, output_size: int
     ):
         super(AbstractNCARule, self).__init__()
         self.device = device
         self.input_size = input_size
-        self.latent_size = latent_size
+        self.hidden_size = hidden_size
         self.output_size = output_size
 
     @abc.abstractmethod
