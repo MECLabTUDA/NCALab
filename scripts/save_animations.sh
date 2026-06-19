524bf6fe6e7eb5fa278e7ce710012f82cae2dd79
Henry Krumb <mail@henrykrumb.de>
2026-06-19 17:02:40 +0200

    save animations script: run without GPU

--- scripts/save_animations.sh
+++ scripts/save_animations.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -e
 
-uv run tasks/class_cifar10/eval_class_cifar10.py
-uv run tasks/growing_emoji/eval_growing_emoji.py
-uv run tasks/class_medmnist/eval_class_dermamnist.py
-uv run tasks/class_medmnist/eval_class_bloodmnist.py
-uv run tasks/class_medmnist/eval_class_pathmnist.py
-uv run tasks/segmentation_kvasir_seg/eval_segmentation_kvasir_seg.py
-uv run tasks/selfclass_mnist/eval_selfclass_mnist.py
+uv run tasks/class_cifar10/eval_class_cifar10.py --no-gpu
+uv run tasks/growing_emoji/eval_growing_emoji.py --no-gpu
+uv run tasks/class_medmnist/eval_class_dermamnist.py --no-gpu
+uv run tasks/class_medmnist/eval_class_bloodmnist.py --no-gpu
+uv run tasks/class_medmnist/eval_class_pathmnist.py --no-gpu
+uv run tasks/segmentation_kvasir_seg/eval_segmentation_kvasir_seg.py --no-gpu
+uv run tasks/selfclass_mnist/eval_selfclass_mnist.py --no-gpu
 
 cp tasks/class_medmnist/figures/* artwork
 cp tasks/class_cifar10/figures/* artwork
